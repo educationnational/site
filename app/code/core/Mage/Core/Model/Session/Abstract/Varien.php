6e8ee152205aea33935743ad947091a931274e1c
Jonathan Ingram <jonathan.b.ingram@gmail.com>
2011-10-06 08:09:09 +1100

    Went through Magento install process

--- app/code/core/Mage/Core/Model/Session/Abstract/Varien.php
+++ app/code/core/Mage/Core/Model/Session/Abstract/Varien.php
@@ -87,7 +87,7 @@ class Mage_Core_Model_Session_Abstract_Varien extends Varien_Object
             'httponly' => $cookie->getHttponly()
         );
 
-        if (!$cookieParams['httponly']) {
+        /*if (!$cookieParams['httponly']) {
             unset($cookieParams['httponly']);
             if (!$cookieParams['secure']) {
                 unset($cookieParams['secure']);
@@ -99,7 +99,7 @@ class Mage_Core_Model_Session_Abstract_Varien extends Varien_Object
 
         if (isset($cookieParams['domain'])) {
             $cookieParams['domain'] = $cookie->getDomain();
-        }
+        }*/
 
         call_user_func_array('session_set_cookie_params', $cookieParams);
 
